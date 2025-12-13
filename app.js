3862534566c0efbe446760d4120b15bca6c2f540
Soen Razatovo <soen.razatovo@gmail.com>
2025-12-13 11:46:32 +0100

    Creation of boutique.html layout

--- app.js
+++ app.js
@@ -1,29 +1,50 @@
-const smoke = document.querySelector("main section:nth-child(1) img:nth-child(2)");
-let degree = 0;
+let path = window.location.pathname;
+let page = path.split("/").pop();
 
-function rotate(){
-    step = 0.1;
-    degree += step;
-    if (degree > 360) {
-        degree -= 360;
+if (page === "index.html"){
+    const smoke = document.querySelector("main section:nth-child(1) img:nth-child(2)");
+    let degree = 0;
+    
+    function rotate(){
+        step = 0.1;
+        degree += step;
+        if (degree > 360) {
+            degree -= 360;
+        }
+        smoke.style.transform = 'rotate(-' + degree + 'deg)';
+        setTimeout(rotate, 20);   
     }
-    smoke.style.transform = 'rotate(-' + degree + 'deg)';
-    setTimeout(rotate, 20);   
+    
+    rotate();
+    
+    const left_arrow = document.getElementById("left-arrow");
+    const right_arrow = document.getElementById("right-arrow");
+    const carousel = document.getElementsByClassName("carousel")[0];
+    
+    function scrollLeft(){
+        carousel.scrollBy({top:0, left:-300, behavior:'smooth'});
+    }
+    
+    function scrollRight(){
+        carousel.scrollBy({top:0, left:300, behavior:'smooth'});
+    }
+    
+    left_arrow.addEventListener("click", scrollLeft);
+    right_arrow.addEventListener("click", scrollRight);
 }
 
-rotate();
-
-const left_arrow = document.getElementById("left-arrow");
-const right_arrow = document.getElementById("right-arrow");
-const carousel = document.getElementsByClassName("carousel")[0];
-
-function scrollLeft(){
-    carousel.scrollBy({top:0, left:-300, behavior:'smooth'});
+if (page === "boutique.html"){
+    const searchbar = document.querySelector("#searchbar input");
+    const names = document.querySelectorAll(".article h2:nth-child(2)");
+    console.log(names);
+    searchbar.addEventListener("input", () => {
+        for(let i=0; i<names.length; i++){
+            // console.log(names[i].innerHTML);
+            if (names[i].innerHTML.toLowerCase().includes(searchbar.value.toLowerCase())){
+                names[i].parentElement.style.display = "flex";
+            }else{
+                names[i].parentElement.style.display = "none";
+            }
+        }
+    });
 }
-
-function scrollRight(){
-    carousel.scrollBy({top:0, left:300, behavior:'smooth'});
-}
-
-left_arrow.addEventListener("click", scrollLeft);
-right_arrow.addEventListener("click", scrollRight);
